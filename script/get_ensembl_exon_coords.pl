3279891668a7022d92a296b5e73e1702ada50608
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2015-03-23 12:08:37 +0000

    Rewrite with less complexity and more subs

--- script/get_ensembl_exon_coords.pl
+++ script/get_ensembl_exon_coords.pl
@@ -1,5 +1,3 @@
-#!/usr/bin/perl
-
 use Text::CSV;
 use Bio::EnsEMBL::Registry;
 use feature qw/ say /;
@@ -14,119 +12,156 @@ my $pre_region_param = 150; # unless overridden below
 my $post_region_param = 150;
 
 GetOptions(
-    'input=s'         	=> \$input_file_param,
-    'output=s'			=> \$output_file_param,
-    'species=s'       	=> \$species_param,
-    'pre_region=i'		=> \$pre_region_param,
-    'post_region=i'		=> \$post_region_param,
+    'input=s'           => \$input_file_param,
+    'output=s'          => \$output_file_param,
+    'species=s'         => \$species_param,
+    'pre_region=i'      => \$pre_region_param,
+    'post_region=i'     => \$post_region_param,
 ) or die usage_message();
+
+
+my $out_array_ref = do_main_flow();
+
+generate_output_csv( $out_array_ref );
+
+exit;
+
+#############################
+#subroutines
+#############################
 
 sub usage_message {
     print <<'END_USAGE';
 usage: get_ensembl_coords.pl
-	--input=<input.csv>
-	--output=<output.csv>
-	--species=[ Human | Mouse ]
-	--pre_region=150
-	--post_region=150
+    --input=<input.csv>
+    --output=<output.csv>
+    --species=[ Human | Mouse ]
+    --pre_region=150
+    --post_region=150
 END_USAGE
 return;
 }
 
-my $doomed;
+sub check_params {
+    my $doomed;
 
-unless ($input_file_param) {
-	print "Specify input_file\n";
-	$doomed++;
-}
-unless ($output_file_param) {
-	print "Specify output_file\n";
-	$doomed++;
-}
-unless ($species_param) {
-	print "Specify species\n";
-	$doomed++;
-}
-unless ($pre_region_param) {
-	print "Specify pre_region\n";
-	$doomed++;
-}
-unless ($post_region_param) {
-	print "Specify post_region\n";
-	$doomed++;
-}
-if ($doomed) {
-	usage_message();
-	die;
+    unless ($input_file_param) {
+       print "Specify input_file\n";
+       $doomed++;
+   }
+   unless ($output_file_param) {
+       print "Specify output_file\n";
+       $doomed++;
+   }
+   unless ($species_param) {
+       print "Specify species\n";
+       $doomed++;
+   }
+   unless ($pre_region_param) {
+       print "Specify pre_region\n";
+       $doomed++;
+   }
+   unless ($post_region_param) {
+       print "Specify post_region\n";
+       $doomed++;
+   }
+   if ($doomed) {
+       usage_message();
+       die;
+   }
 }
 
-my @gene_array;
-my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
+sub get_gene_exons_from_file {
+
+    my @gene_array;
+    my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
                 or die "Cannot use CSV: ".Text::CSV->error_diag ();
- 
-open my $fh, "<:encoding(utf8)", $input_file_param or die $input_file_param . ": $!";
-while ( my $gene_row = $csv->getline( $fh ) ) {
-    next if $gene_row->[0] eq 'Gene ID'; # deals with the header line
-    push @gene_array, $gene_row;
+
+    open my $fh, "<:encoding(utf8)", $input_file_param or die $input_file_param . ": $!";
+    while ( my $gene_row = $csv->getline( $fh ) ) {
+        next if $gene_row->[0] eq 'Gene ID'; # deals with the header line
+        push @gene_array, $gene_row;
+    }
+    $csv->eof or $csv->error_diag();
+    close $fh;
+    return \@gene_array;
 }
-$csv->eof or $csv->error_diag();
-close $fh;
 
-say 'Read ' . scalar @gene_array . ' rows from ' . $input_file_param;
+sub generate_output_csv {
+    my $out_ref = shift;
 
-say 'Connecting to the ensembl registry';
+    my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
+                or die "Cannot use CSV: ".Text::CSV->error_diag ();
+    $csv->eol("\n");
+    open my $out_fh, ">:encoding(utf8)", $output_file_param or die $output_file_param . ": $!";
+    $csv->print ($out_fh, $_) for @{$out_array};
+    close $out_fh or die $output_file_param . ": $!";
+}
 
-my $registry = 'Bio::EnsEMBL::Registry';
+sub do_main_flow {
+    my $gene_array = get_gene_exons_from_file();
 
-$registry->load_registry_from_db(
+    say 'Read ' . scalar @$gene_array . ' rows from ' . $input_file_param;
+
+    say 'Connecting to the ensembl registry';
+
+    my $registry = 'Bio::EnsEMBL::Registry';
+
+    $registry->load_registry_from_db(
         -host => $ENV{LIMS2_ENSEMBL_HOST} || 'ensembldb.ensembl.org',
         -user => $ENV{LIMS2_ENSEMBL_USER} || 'anonymous'
-    );
+        );
 
-my $exon_adaptor = $registry->get_adaptor($species, 'Core', 'Exon');
+    my $exon_adaptor = $registry->get_adaptor($species, 'Core', 'Exon');
 
-my $slice_adaptor = $registry->get_adaptor($species, 'Core', 'Slice');
+    my $slice_adaptor = $registry->get_adaptor($species, 'Core', 'Slice');
 
-my @out_array;
-push @out_array,
-	[	'Gene Id',
-		'Exon Id',
-		'chr_start',
-		'chr_end',
-		'pre_chr_name',
-		'pre_chr_start',
-		'pre_chr_end',
-		'post_chr_name',
-		'post_chr_start',
-		'post_chr_end',
-		];
+    my @out_array;
+    # generate headers for the output csv
+    push @out_array,
+    [   'Gene Id',
+        'Exon Id',
+        'chr_start',
+        'chr_end',
+        'pre_chr_name',
+        'pre_chr_start',
+        'pre_chr_end',
+        'post_chr_name',
+        'post_chr_start',
+        'post_chr_end',
+    ];
 
-foreach my $gene_row ( @gene_array ) {
-    my $gene_id = $gene_row->[0];
-    my $exon_id = $gene_row->[1];
-$DB::single=1;
-    my $exon = $exon_adaptor->fetch_by_stable_id( $exon_id );
-    if ( ! $exon ) {
-    	say 'No exon stable id found: ' . $exon_id . ' (' . $gene_id . ')';
-    	next; 
-    }
+    my $coord_data = generate_coords( $gene_array );
+    return \@out_array;
+}
 
-    my $ov = {};
+sub generate_coords {
+    foreach my $gene_row ( @gene_array ) {
+        my $gene_id = $gene_row->[0];
+        my $exon_id = $gene_row->[1];
+        $DB::single=1;
+        my $exon = $exon_adaptor->fetch_by_stable_id( $exon_id );
+        if ( ! $exon ) {
+            say 'No exon stable id found: ' . $exon_id . ' (' . $gene_id . ')';
+            next;
+        }
 
-	say 'Gene: ' . $gene_id;
-    say 'Exon: ' . $exon_id;
+        my $ov = {};
 
-    $ov->{gene_id} = $gene_id;
-    $ov->{exon_id} = $exon_id;
- 	$ov->{pre_chr_name} = $exon->slice->seq_region_name();
- 	$ov->{exon_chr_start} = $exon->start;
-    $ov->{pre_chr_start} = $exon->start - ($pre_region_param + 1); # starts at 1 not zero
-    $ov->{pre_chr_end} = $exon->start - 1; # parameterize this?
+        say 'Gene: ' . $gene_id;
+        say 'Exon: ' . $exon_id;
 
-    $ov->{exon_chr_end} = $exon->end;
-    $ov->{post_chr_name} = $exon->slice->seq_region_name;
-    $ov->{post_chr_start} = $exon->end + 1; # parameterize this?
-    $ov->{post_chr_end} = $exon->end + $post_region_param;
+        $ov->{gene_id} = $gene_id;
+        $ov->{exon_id} = $exon_id;
+        $ov->{pre_chr_name} = $exon->slice->seq_region_name();
+        $ov->{exon_chr_start} = $exon->start;
+        $ov->{pre_chr_start} = $exon->start - ($pre_region_param + 1); # starts at 1 not zero
+        $ov->{pre_chr_end} = $exon->start - 1; # parameterize this?
+
+        $ov->{exon_chr_end} = $exon->end;
+        $ov->{post_chr_name} = $exon->slice->seq_region_name;
+        $ov->{post_chr_start} = $exon->end + 1; # parameterize this?
+        $ov->{post_chr_end} = $exon->end + $post_region_param;
 
     # Now get a slice that covers the whole region we have defined and check if there are any other exons
     # that overlap that region.
@@ -136,54 +171,56 @@ $DB::single=1;
     my $transcripts = $slice->get_all_Transcripts();
 
     foreach my $trans ( @{$transcripts} ) {
-        say '-- transcript: ' . $trans->display_id;
-        my $exons = $trans->get_all_Exons();
+        process_transcript( $trans );
 
-        foreach my $sample_exon ( @{$exons}){
-            say '- sample_exon id: ' . $sample_exon->display_id;
-            if ( $sample_exon->display_id eq $ov->{exon_id} ) {
-                say '++ This exon is the critical exon in the same transcript';
-                next;
-            }
-            my $trans_exon = $sample_exon->transform('chromosome');
-            if (my $new_left = is_left_overlap( $trans_exon, $ov))
-             {
-                $DB::single=1;
-                say '+ Found pre_exon overlap with ' . $trans_exon->display_id;
-                say '+ old pre_chr_start was: ' . $ov->{pre_chr_start};
-                $ov->{pre_chr_start} = $new_left + 1;
-                say '+ new pre_chr_start is:  ' . $ov->{pre_chr_start};
+        }
+
+        push @out_array, [
+        $ov->{gene_id},
+        $ov->{exon_id},
+        $ov->{exon_chr_start},
+        $ov->{exon_chr_end},
+        $ov->{pre_chr_name},
+        $ov->{pre_chr_start},
+        $ov->{pre_chr_end},
+        $ov->{post_chr_name},
+        $ov->{post_chr_start},
+        $ov->{post_chr_end},
+        ];
+    }
+}
+
+sub process_transcript {
+    my transcript = shift;
+
+    say '-- transcript: ' . $transcript->display_id;
+    my $exons = $transcript->get_all_Exons();
+
+    foreach my $sample_exon ( @{$exons}){
+        say '- sample_exon id: ' . $sample_exon->display_id;
+        if ( $sample_exon->display_id eq $ov->{exon_id} ) {
+            say '++ This exon is the critical exon in the same transcript';
+            next;
+        }
+        my $trans_exon = $sample_exon->transform('chromosome');
+        if (my $new_left = is_left_overlap( $trans_exon, $ov))
+        {
+            say '+ Found pre_exon overlap with ' . $trans_exon->display_id;
+            say '+ old pre_chr_start was: ' . $ov->{pre_chr_start};
+            $ov->{pre_chr_start} = $new_left + 1;
+            say '+ new pre_chr_start is:  ' . $ov->{pre_chr_start};
             } elsif ( my $new_right = is_right_overlap( $trans_exon, $ov) ) {
-                $DB::single=1;
+
                 say ' Found post_exon overlap ' . $trans_exon->display_id;
                 say '+ old post_chr_end was: ' . $ov->{post_chr_end};
                 $ov->{post_chr_end} = $new_right - 1;
                 say '+ new post_chr_end is:  ' . $ov->{post_chr_end};
             }
         }
-        say '---';
-    }
-    
-    push @out_array, [
-    		$ov->{gene_id},
-    		$ov->{exon_id},
-    		$ov->{exon_chr_start},
-    		$ov->{exon_chr_end},
-    		$ov->{pre_chr_name},
-    		$ov->{pre_chr_start},
-    		$ov->{pre_chr_end},
-    		$ov->{post_chr_name},
-    		$ov->{post_chr_start},
-    		$ov->{post_chr_end},
-    		];
+    say '---';
+
+    return; # should return something to push onto the output array
 }
-
-$csv->eol("\n");
-open $fh, ">:encoding(utf8)", $output_file_param or die $output_file_param . ": $!";
-$csv->print ($fh, $_) for @out_array;
-close $fh or die $output_file_param . ": $!";
-
-
 
 # Needs a diagram!
 #
